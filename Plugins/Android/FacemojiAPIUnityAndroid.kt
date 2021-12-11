39565baae0cb6bc32901ba1e10263158aee47d5d
BennyKok <itechbenny@gmail.com>
2021-12-11 14:53:51 +0800

    feat: add blend shape name and input callback

--- Plugins/Android/FacemojiAPIUnityAndroid.kt
+++ Plugins/Android/FacemojiAPIUnityAndroid.kt
@@ -2,11 +2,24 @@ package co.facemoji.mocap4face
 
 import android.content.Context
 import android.util.Log
+import android.view.WindowManager
 import co.facemoji.api.FacemojiAPI
 import co.facemoji.io.ApplicationContext
+import co.facemoji.math.Quaternion
+import co.facemoji.tracker.FaceTrackerResult
+import co.facemoji.tracker.OpenGLTexture
+import co.facemoji.ui.CameraTextureView
+import kotlin.math.max
+import kotlin.math.min
 
 class FacemojiAPIUnityAndroid {
+
+    private var cameraTracker: CameraTracker? = null
+    private var onActivateListener: OnActivateListener? = null;
+
     fun initialize(apiKey: String, context: Context, onActivateListener: OnActivateListener) {
+        this.onActivateListener = onActivateListener;
+
         Log.v("Facemoji: ", "Facemoji: Initialize in Native")
         FacemojiAPI.initialize(apiKey, ApplicationContext(context.applicationContext))
             .whenDone { activated ->
@@ -15,11 +28,58 @@ class FacemojiAPIUnityAndroid {
             }
     }
 
-    fun createCameraTracker() {
-        
+    /**
+     * Converts head rotation to blendshape-like coefficients to display in the UI
+     */
+    private fun faceRotationToSliders(rotation: Quaternion): Map<String, Float> {
+        val euler = rotation.toEuler()
+        val halfPi = Math.PI.toFloat() * 0.5f
+        return mapOf(
+            "headLeft" to max(0f, euler.y) / halfPi,
+            "headRight" to -min(0f, euler.y) / halfPi,
+            "headUp" to -min(0f, euler.x) / halfPi,
+            "headDown" to max(0f, euler.x) / halfPi,
+            "headRollLeft" to -min(0f, euler.z) / halfPi,
+            "headRollRight" to max(0f, euler.z) / halfPi,
+        )
+    }
+
+    private fun onTracker(cameraImage: OpenGLTexture, trackerResult: FaceTrackerResult?) {
+        if (trackerResult != null) {
+            val blendshapes = trackerResult.blendshapes +
+                    faceRotationToSliders(trackerResult.rotationQuaternion)
+
+            onActivateListener?.onBlendShapeValues(blendshapes)
+        } else {
+            onActivateListener?.onBlendShapeValues(emptyMap())
+        }
+    }
+
+    fun createCameraTracker(context: Context) {
+        val cameraTracker = CameraTracker(context)
+        cameraTracker.trackerDelegate = this::onTracker
+        cameraTracker.blendshapeNames.whenDone { names ->
+            val headPoseNames = faceRotationToSliders(Quaternion.identity).keys
+            onActivateListener?.onBlendShapeNames((names + headPoseNames).sorted());
+        }
+        this.cameraTracker = cameraTracker
+    }
+
+    fun pause() {
+        cameraTracker?.stop()
+    }
+
+    fun resume() {
+        cameraTracker?.restart()
+    }
+
+    fun destroy() {
+        cameraTracker?.stop()
     }
 
     interface OnActivateListener {
         fun onActivate(activated: Boolean)
+        fun onBlendShapeNames(names: List<String>)
+        fun onBlendShapeValues(input: Map<String, Float>)
     }
 }
--- Runtime/FacemojiAPI.cs
+++ Runtime/FacemojiAPI.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,21 +11,31 @@ public class FacemojiAPI
     private static readonly string facemojiBridgeClassName = "co.facemoji.mocap4face.FacemojiAPIUnityAndroid";
     private AndroidJavaObject facemojiAPIClass;
     private AndroidJavaClass unityAndroidPlayer;
-    private AndroidJavaObject cameraTracker;
     private AndroidJavaObject activity;
+    private bool isCameraCreated = false;
 
 
     class OnActivateListener : AndroidJavaProxy
     {
-        public Action<bool> onActivateCallback;
-        public OnActivateListener(Action<bool> onActivate) : base("co.facemoji.mocap4face.FacemojiAPIUnityAndroid$OnActivateListener")
+        public Action<bool> onActivateAction;
+        public Action<List<string>> onBlendShapeNamesAction;
+        public Action<Dictionary<string, float>> onBlendShapeValuesAction;
+
+        public OnActivateListener(
+            Action<bool> onActivateAction,
+            Action<List<string>> onBlendShapeNamesAction,
+            Action<Dictionary<string, float>> onBlendShapeValuesAction
+        ) : base(
+            "co.facemoji.mocap4face.FacemojiAPIUnityAndroid$OnActivateListener")
         {
-            onActivateCallback = onActivate;
+            this.onActivateAction = onActivateAction;
+            this.onBlendShapeNamesAction = onBlendShapeNamesAction;
+            this.onBlendShapeValuesAction = onBlendShapeValuesAction;
         }
-        void onActivate(bool activated)
-        {
-            onActivateCallback?.Invoke(activated);
-        }
+
+        void onActivate(bool activated) => onActivateAction?.Invoke(activated);
+        void onBlendShapeNames(List<string> names) => onBlendShapeNamesAction?.Invoke(names);
+        void onBlendShapeValues(Dictionary<string, float> input) => onBlendShapeValuesAction?.Invoke(input);
     }
 #endif
 
@@ -40,11 +52,16 @@ public class FacemojiAPI
         }
     }
 
-    private FacemojiAPI() { }
+    private FacemojiAPI()
+    {
+    }
 
     public void Initialize(string apiKey)
     {
-#if UNITY_ANDROID && !UNITY_EDITOR
+        if (Application.isEditor)
+            return;
+
+#if UNITY_ANDROID
         Debug.Log("Facemoji: Initialize from Unity");
         facemojiAPIClass = new AndroidJavaObject(facemojiBridgeClassName);
         unityAndroidPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
@@ -53,26 +70,82 @@ public class FacemojiAPI
         facemojiAPIClass.Call("initialize",
             apiKey,
             activity,
-            new OnActivateListener((activated) =>
-            {
-                if (activated)
-                {
-                    Debug.Log("Facemoji: API activation was successful");
-                }
-                else
-                {
-                    Debug.Log("Facemoji: API activation failed");
-                }
-            })
+            new OnActivateListener(OnActivate, OnBlendShapeNames, OnBlendShapeValues)
         );
 #endif
     }
 
+    void OnActivate(bool activated)
+    {
+        if (activated)
+        {
+            Debug.Log("Facemoji: API activation was successful");
+        }
+        else
+        {
+            Debug.Log("Facemoji: API activation failed");
+        }
+    }
+
+    void OnBlendShapeNames(List<string> names)
+    {
+        var result = names.Aggregate("Facemoji: OnBlendShapeNames: ", (current, item) => current + (item + ", "));
+        Debug.Log(result);
+    }
+
+    void OnBlendShapeValues(Dictionary<string, float> input)
+    {
+        var result = input.Aggregate("Facemoji: OnBlendShapeValues: ",
+            (current, item) => current + ($"{item.Key} : {item.Value}" + "\n"));
+        Debug.Log(result);
+    }
+
+
+    public void Pause()
+    {
+        if (Application.isEditor)
+            return;
+
+#if UNITY_ANDROID
+        facemojiAPIClass.Call("pause");
+#endif
+        Debug.Log("Facemoji: Pause");
+    }
+
+    public void Resume()
+    {
+        if (Application.isEditor)
+            return;
+
+        if (!isCameraCreated)
+        {
+            CreateCameraTracker();
+        }
+#if UNITY_ANDROID
+        facemojiAPIClass.Call("resume");
+#endif
+        Debug.Log("Facemoji: Resume");
+    }
+
+    public void Destroy()
+    {
+        if (Application.isEditor)
+            return;
+
+#if UNITY_ANDROID
+        facemojiAPIClass.Call("destroy");
+#endif
+        Debug.Log("Facemoji: Destroy");
+    }
+
     public void CreateCameraTracker()
     {
+        if (Application.isEditor)
+            return;
+
 #if UNITY_ANDROID
-        cameraTracker = new AndroidJavaObject("co.facemoji.io.ApplicationContext", activity);
-        // cameraTracker.Call();
+        facemojiAPIClass.Call("Facemoji: CreateCameraTracker", activity);
+        isCameraCreated = true;
 #endif
     }
 }
